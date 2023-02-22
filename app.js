2174af91fca945262cf7022ae8fa7198032b4499
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-02-22 21:13:33 +0100

    stabilise front end app and thiers requirement

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
  var express=require('express');
- 
+var path=require('path');
+
 const TodoController = require('./Controllers/TodoController');
 
  var app=express();
@@ -7,13 +8,14 @@ const TodoController = require('./Controllers/TodoController');
 
  app.set('view engine','ejs');
 
- app.use(express.static('./public'));
+ app.use('/assets',express.static('public/assets'))
 
  TodoController(app);
 
  app.listen(3000);
  
  console.log('open browser at 127.0.0.1:3000 to see the job');
+ console.log(__dirname+'/public');
 
 
 
