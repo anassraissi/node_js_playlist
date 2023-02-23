3e3ae550aed3015d290156893d9cfe2a2c547d7d
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-02-23 21:00:38 +0100

    make insert and delete item to the todolist app

--- Controllers/TodoController.js
+++ Controllers/TodoController.js
@@ -1,13 +1,24 @@
 module.exports=function(app){
+    var bodyParser=require('body-parser');
+    var UrlEncodeParser=bodyParser.urlencoded({extended:false});
 
+    var data=[{item:'make breakfast'},{item:'make lunch'},{item :'take sleep'}];
     app.get('/todo',function(req,res){
-    res.render('todo');
+    res.render('todo',{get_data:data});
     });
 
-    app.post('/todo',function(req,res){
-
+    app.post('/todo',UrlEncodeParser,function(req,res){
+        data.push(req.body);
+        res.json(data); 
     });
-    app.delete('/todo',function(req,res){
+    app.delete('/todo/:item',function(req,res){
+        data=data.filter(function(todo){
+            return todo.item.replace(/ /g, "-") !== req.params.item; 
+
+            // replace ghay3awad espaceb  b -
+            // il kan true ghay returnih daba hna ghayretuerni ghir il makaychbahch dak li siftnah fparametre
+        })
+        res.json(data); 
 
     });
 
