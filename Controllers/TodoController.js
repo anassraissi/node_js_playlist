c98fad86ceed0605c307f2d33f09863b9fc836f2
ANASS RAISSI <88003906+anassraissi@users.noreply.github.com>
2023-03-06 19:15:38 +0100

    End TodoList

--- Controllers/TodoController.js
+++ Controllers/TodoController.js
@@ -1,12 +1,45 @@
 module.exports=function(app){
     var bodyParser=require('body-parser');
     var UrlEncodeParser=bodyParser.urlencoded({extended:false});
+    var mongoose=require('mongoose');
+    
+    mongoose.set('strictQuery', true);
+
+    mongoose.connect(
+         'mongodb+srv://DbUser1:123@cluster0.29h1u6s.mongodb.net/?retryWrites=true&w=majority',
+        {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+          useCreateIndex: true,
+        },
+        (err,client) => {
+            if(err) console.log('err');
+            else console.log("mongdb is connected");
+        }
+      );
+      var TodoSchema = new mongoose.Schema({
+        item: String
+    });
+    
+
+    // var Todo=mongoose.model('Todo',TodoSchema);
+    // var itemone=Todo({item:'pick a flower'}).save(function(err){    
+    //     if(err) throw err;
+    //     console.log('data saved');
+    // })
+
+    async function run() {
+        var Todo=mongoose.model('Todo',TodoSchema);
+      
+    var result= await mongoose.model('Todo').findOne(); // Works!
+    console.log(result);
+      }
+      run();
 
     var data=[{item:'make breakfast'},{item:'make lunch'},{item :'take sleep'}];
     app.get('/todo',function(req,res){
     res.render('todo',{get_data:data});
     });
-
     app.post('/todo',UrlEncodeParser,function(req,res){
         data.push(req.body);
         res.json(data); 
